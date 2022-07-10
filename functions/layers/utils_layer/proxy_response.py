1565f42f161a6ab5851bc39bf71a942603030299
mirumi <mail@mirumi.tech>
2022-07-10 17:15:33 +0900

    add: 400 and 401

--- functions/layers/utils_layer/proxy_response.py
+++ functions/layers/utils_layer/proxy_response.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import TypedDict
+from typing import Any, cast, Literal, TypedDict
 
 import os
 import json
@@ -7,12 +7,10 @@ import dynamodb
 
 API_ALLOW_ORIGIN = os.getenv("API_ALLOW_ORIGIN")
 
-# types
 class ProxyResponse(TypedDict):
     statusCode: int
     headers: dict[str, str | None]
     body: str
-
 
 def s200(val: object = None) -> ProxyResponse:
     return {
@@ -23,9 +21,20 @@ def s200(val: object = None) -> ProxyResponse:
         "body": json.dumps(
             val if val is not None else "no response data",
             default=dynamodb.decimal_to_float,
-        )
+        ),
     }
 
+def s400() -> ProxyResponse:
+    return {
+        "statusCode": 400,
+        "body": "required request data is missing or I/F schema is invalid",
+    }
+
+def s403() -> ProxyResponse:
+    return {
+        "statusCode": 403,
+        "body": "required api key is missing or invalid",
+    }
 
 def s500(val: object = None) -> ProxyResponse:
     return {
@@ -36,5 +45,5 @@ def s500(val: object = None) -> ProxyResponse:
         "body": json.dumps(
             val if val is not None else "no response data",
             default=dynamodb.decimal_to_float,
-        )
+        ),
     }
